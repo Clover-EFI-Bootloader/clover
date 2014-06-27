Clover-EFI-Bootloader/clover

commit 0c8a1dad07c8bafc1532f5325921d3b6e8cca811

Simplifying some codes
git-svn-id: svn+ssh://svn.code.sf.net/p/cloverefiboot/code@2738 52d7a1c0-eba5-4e63-a9d7-e3186cd7c462

rEFIt_UEFI/Platform/kext_inject.c
@@ -238,12 +238,13 @@ EFI_STATUS LoadKexts(IN LOADER_ENTRY *Entry)
 		archCpuType = CPU_TYPE_X86_64;
 	} else if (Arch != NULL && StrnCmp(Arch,L"i386",StrLen(L"i386")) == 0) {
 		archCpuType = CPU_TYPE_I386;
-	} else if (Entry->OSVersion != NULL && AsciiStrnCmp(Entry->OSVersion,"10.",3) == 0) {
-		if ((Entry->OSVersion[3] >= '8') || (Entry->OSVersion[3] == '1')) {  //assume 10.10 - 10.19
-			archCpuType = CPU_TYPE_X86_64; // For >= 10.8, only x86_64 exists
-		} else if (Entry->OSVersion[3] >= '3' && Entry->OSVersion[3] <= '6') {
-			archCpuType = CPU_TYPE_I386; // For <= 10.6, use default of i386
-		}
+	} else if (Entry->OSVersion != NULL) {
+	  UINT16 os_version = AsciiOSVersionToUint64(Entry->OSVersion);
+	  if (os_version >= AsciiOSVersionToUint64("10.8")) {
+		archCpuType = CPU_TYPE_X86_64; // For OSVersion >= 10.8, only x86_64 exists
+	  } else if (os_version <= AsciiOSVersionToUint64("10.6")) {
+		archCpuType = CPU_TYPE_I386; // For OSVersion <= 10.6, use default of i386
+	  }
 	}
 
 //	Volume = Entry->Volume;

rEFIt_UEFI/Platform/FixBiosDsdt.c
@@ -3595,7 +3595,7 @@ UINT32 AddHDEF (UINT8 *dsdt, UINT32 len, CHAR8* OSVersion)
     aml_add_string_buffer(pack, "onboard-1");
   }
   if (!AddProperties(pack, DEV_HDA)) {
-    if ((OSVersion != NULL && AsciiStrnCmp(OSVersion, "10.", 3) == 0 && OSVersion[3] >= '0' && OSVersion[3] <= '7') || (gSettings.HDALayoutId > 0)) {
+    if ((OSVersion != NULL && AsciiOSVersionToUint64(OSVersion) <= AsciiOSVersionToUint64("10.7")) || (gSettings.HDALayoutId > 0)) {
       aml_add_string(pack, "layout-id");
       aml_add_byte_buffer(pack, (CHAR8*)&HDAlayoutId, 4);
     }

rEFIt_UEFI/Platform/device_inject.c
@@ -689,7 +689,7 @@ BOOLEAN set_hda_props(EFI_PCI_IO_PROTOCOL *PciIo, pci_dt_t *hda_dev, CHAR8 *OSVe
                         gSettings.AddProperties[i].ValueLen);
     }
     if (!Injected) {
-      if ((OSVersion != NULL && AsciiStrnCmp(OSVersion, "10.", 3) == 0 && OSVersion[3] >= '0' && OSVersion[3] <= '7') || (gSettings.HDALayoutId > 0)) {
+      if ((OSVersion != NULL && AsciiOSVersionToUint64(OSVersion) <= AsciiOSVersionToUint64("10.7")) || (gSettings.HDALayoutId > 0)) {
         devprop_add_value(device, "layout-id", (UINT8*)&layoutId, 4);
       }
       layoutId = 0; // reuse variable
@@ -701,6 +701,6 @@ BOOLEAN set_hda_props(EFI_PCI_IO_PROTOCOL *PciIo, pci_dt_t *hda_dev, CHAR8 *OSVe
     }
     
   }
-  
-	return TRUE;
+
+  return TRUE;
 }

rEFIt_UEFI/entry_scan/loader.c
@@ -673,8 +673,7 @@ STATIC VOID AddDefaultMenu(IN LOADER_ENTRY *Entry)
 
   // Only kernels up to 10.7 have 32-bit mode
   KernelIs64BitOnly = (Entry->OSVersion == NULL ||
-                       (Entry->OSVersion[3] > '7') ||
-                       (Entry->OSVersion[3] == '1'));
+                       AsciiOSVersionToUint64(Entry->OSVersion) >= AsciiOSVersionToUint64("10.8"));
   
   FileName = Basename(Entry->LoaderPath);
   

commit 12b765a4406ba29afe660b4be8211a842abdea0c

Correct problem with kext inject introduced with r2738
git-svn-id: svn+ssh://svn.code.sf.net/p/cloverefiboot/code@2742 52d7a1c0-eba5-4e63-a9d7-e3186cd7c462

rEFIt_UEFI/Platform/kext_inject.c
@@ -239,7 +239,7 @@ EFI_STATUS LoadKexts(IN LOADER_ENTRY *Entry)
 	} else if (Arch != NULL && StrnCmp(Arch,L"i386",StrLen(L"i386")) == 0) {
 		archCpuType = CPU_TYPE_I386;
 	} else if (Entry->OSVersion != NULL) {
-	  UINT16 os_version = AsciiOSVersionToUint64(Entry->OSVersion);
+	  UINT64 os_version = AsciiOSVersionToUint64(Entry->OSVersion);
 	  if (os_version >= AsciiOSVersionToUint64("10.8")) {
 		archCpuType = CPU_TYPE_X86_64; // For OSVersion >= 10.8, only x86_64 exists
 	  } else if (os_version < AsciiOSVersionToUint64("10.7")) {